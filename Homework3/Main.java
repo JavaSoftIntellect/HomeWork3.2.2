71fc8d387721a95922d12191e0efec26400d05a2
Remzi <remzitdf@abv.bg>
2016-05-18 19:32:22 +0300

    ok

---
+++ Homework3/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter N:");
+		int a = in.nextInt();
+		for (int i = 1; i <= a; i++) {
+			System.out.print(i + " ");
+		}
+
+
+	}
+
+}
+
