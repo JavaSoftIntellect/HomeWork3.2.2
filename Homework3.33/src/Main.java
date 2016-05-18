cfeeadf37dc5175c9d9107a2a3b09e46f2b36d7f
Remzi <remzitdf@abv.bg>
2016-05-18 20:14:40 +0300

    ok

---
+++ Homework3.33/src/Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter N:");
+		int n = in.nextInt();
+		System.out.print("Enter K:");
+		int k = in.nextInt();
+		int nf = 1, kf = 1, nkf = 1;
+		for (int i = 2; i <= n; i++) {
+			nf *= i;
+		}
+		for (int i = 2; i <= k; i++) {
+			kf *= i;
+		}
+		for (int i = 2; i <= n - k; i++) {
+			nkf *= i;
+		}
+		double result = (nf * kf) / nkf;
+		if (n > k && k > 1)
+			System.out.print(result);
+		else
+			System.out.print("invalid input");
+
+	}
+
+}
