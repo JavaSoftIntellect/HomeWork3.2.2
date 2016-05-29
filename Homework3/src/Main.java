fcd0199059eb11142ff19fd77c053d446f586c5f
Remzi <remzitdf@abv.bg>
2016-05-29 13:07:09 +0300

    Update Main.java

--- Homework3/src/Main.java
+++ Homework3/src/Main.java
@@ -1,8 +1,15 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter N:");
+		int n = in.nextInt();
+		for (int i = 1; i <= n; i++) {
+			if(i%3!=0 && i%7!=0) System.out.println(i);
+		}
+		in.close();
 
 	}
 
