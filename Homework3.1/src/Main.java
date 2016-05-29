85c12d8f5b916f535bde064c9cdef1fde8a10355
Remzi <remzitdf@abv.bg>
2016-05-29 12:58:05 +0300

    Update Main.java

--- Homework3.1/src/Main.java
+++ Homework3.1/src/Main.java
@@ -1,4 +1,18 @@
 
+import java.util.Scanner;
+
 public class Main {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter N:");
+		int n = in.nextInt();
+		for (int i = 1; i <= n; i++) {
+			System.out.println(i);
+		}
+		in.close();
+
+
+	}
 
 }
